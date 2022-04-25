cdbe8e03ab10d23a069de676eaa70a8db9742e6c
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-25 17:35:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CRUD e Atualizações Concorrentes com JPA/Hibernate
+
+## Cadastro de dados únicos com chave composta
+
+Cadastrando ingressos únicos em um sistema de eventos
---
+++ src/main/java/br/com/zup/handora/cadastrandoingressosunicos/CadastrandoIngressosUnicosApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.cadastrandoingressosunicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrandoIngressosUnicosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastrandoIngressosUnicosApplication.class, args);
+    }
+
+}
